8f0464ee8ecd040cca68f2edbc2eaa32649fd702
justinhunt <bitwalkerjapan@gmail.com>
2019-11-21 17:53:25 +0900

    fixed a debug warning when using continue in a switch in a loop

--- classes/diff.php
+++ classes/diff.php
@@ -126,7 +126,7 @@ class diff{
                 switch(count($set)){
                     case 0:
                     case 1:
-                        continue;
+                        break;
                     case 2:
                     default:
                         //clean each word in set
