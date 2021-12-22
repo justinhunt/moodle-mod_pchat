e071ea84ed3d87153fa7cf496e4281ad680b3561
justinhunt <bitwalkerjapan@gmail.com>
2021-12-22 07:22:40 +0000

    added credential fetching

--- lang/en/pchat.php
+++ lang/en/pchat.php
@@ -572,3 +572,9 @@ $string['skinonce'] = 'Once';
 $string['skinupload'] = 'Upload';
 $string['recorderskin'] = 'Recorder Type';
 $string['recorderskindetails'] = 'Choose the type of recorder to show in P-Chat activities';
+
+$string['freetrial'] = "Get Cloud Poodll API Credentials and a Free Trial";
+$string['freetrial_desc'] = "A dialog should appear that allows you to register for a free trial with Poodll. After registering you should login to the members dashboard to get your API user and secret. And to register your site URL.";
+$string['memberdashboard'] = "Member Dashboard";
+$string['memberdashboard_desc'] = "";
+$string['fillcredentials']="Set API user and secret with existing credentials";
