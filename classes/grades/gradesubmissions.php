b0964ab01ae614d8bdb7bc2d7c51188d3317920a
justinhunt <bitwalkerjapan@gmail.com>
2022-06-27 05:29:42 +0000

    float fix #99

--- classes/grades/gradesubmissions.php
+++ classes/grades/gradesubmissions.php
@@ -73,8 +73,8 @@ class gradesubmissions {
                     $onedata->rubricscore =$displaygrades[$onedata->rubricscore];
                 //In the case of decimals, they wont appear in the display grades list, so we mess around removing zeros and building our own equivalent
                 }elseif (count($displaygrades)>1 &&
-                    (is_numeric($onedata->rubricscore ) && is_float($onedata->rubricscore ))){
-                        $onedata->rubricscore = rtrim(((string)$onedata->rubricscore),'0') . '/' . $moduleinstance->grade;
+                    (is_numeric($onedata->rubricscore ) && is_string($onedata->rubricscore ))){
+                        $onedata->rubricscore = rtrim($onedata->rubricscore,'0') . '/' . $moduleinstance->grade;
                 }
             }
             return [$onedata];
